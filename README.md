718b2e43d913f1954392cb059e43d19feba4f8e8
Alexander Gutenkunst <alex.gutenkunst@gmail.com>
2019-11-10 11:54:23 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # photo_rename
 Rename images and videos from booth Google Pixel and iPhone consistently and sortable
+
+Note: Tested with Mac OS, should work with most Unix Systems. Otherwise PR welcome!
+
+## Usage
+This script renames the files in the current directory.
+
+Test with
+```
+photo_rename.sh *.{jpg,mp4,gif} --dry-run
+```
+
+Run with (non-reversible!)
+```
+photo_rename.sh *.{jpg,mp4,gif}
+```
---
+++ photo_rename.sh
@@ -0,0 +1,15 @@
+pixel_time_capture="(?P<hour>[0-9]{2})(?P<min>[0-9]{2})(?P<sec>[0-9]{2})"
+pixel_date_capture="(?P<year>[0-9]{4})(?P<month>[0-9]{2})(?P<day>[0-9]{2})"
+
+pixel_modes_match="[0-9]{5}[ls]{0,1}(?:PORTRAIT|IMG)_[0-9]{5}_BURST${pixel_date_capture}${pixel_time_capture}(?P<stuff>.*)"
+pixel_modes_match2="(?:IMG_|VID_|MVIMG_|PANO_|)${pixel_date_capture}_${pixel_time_capture}(?P<stuff>.*)"
+
+iphone_time_capture="(?P<hour>[0-9]{2}).(?P<min>[0-9]{2}).(?P<sec>[0-9]{2})"
+iphone_date_capture="(?P<year>[0-9]{4})-(?P<month>[0-9]{2})-(?P<day>[0-9]{2})"
+iphone_match="${iphone_date_capture} ${iphone_time_capture}(?P<stuff>.*)"
+
+replace_datetime='$+{year}_$+{month}_$+{day}_$+{hour}-$+{min}-$+{sec}'
+
+rename "s/^$pixel_modes_match$/${replace_datetime}_$+{stuff}/s" "$@"
+rename "s/^$pixel_modes_match2$/$replace_datetime$+{stuff}/s" "$@"
+rename "s/^$iphone_match$/$replace_datetime$+{stuff}/s" "$@"
