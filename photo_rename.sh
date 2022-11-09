6f3caec34004e82dee389c84c892cf506fa50843
Alexander Gutenkunst <alex.gutenkunst+github@gmail.com>
2022-11-09 14:59:29 +0100

    Fixup new google nameing with PXL. Change output names.

--- photo_rename.sh
+++ photo_rename.sh
@@ -6,7 +6,7 @@ pixel_date_capture="(?P<year>[0-9]{4})(?P<month>[0-9]{2})(?P<day>[0-9]{2})"
 whatsapp_date_capture="(?P<year>[0-9]{4})-(?P<month>[0-9]{2})-(?P<day>[0-9]{2})"
 
 pixel_modes_match="[0-9]{5}[lsr]{0,2}(?:PORTRAIT|IMG)_[0-9]{5}_BURST${pixel_date_capture}${pixel_time_capture}(?P<stuff>.*)"
-pixel_modes_match2="(?:IMG_|VID_|MVIMG_|PANO_|)${pixel_date_capture}_${pixel_time_capture}(?P<stuff>.*)"
+pixel_modes_match2="(?:IMG_|VID_|MVIMG_|PANO_|PXL_|)${pixel_date_capture}_${pixel_time_capture}(?P<stuff>.*)"
 pixel_modes_match_screenshot="(?:Screenshot_|)${pixel_date_capture}-${pixel_time_capture}(?P<stuff>.*)"
 
 iphone_time_capture="(?P<hour>[0-9]{2}).(?P<min>[0-9]{2}).(?P<sec>[0-9]{2})"
@@ -15,10 +15,10 @@ iphone_match="${iphone_date_capture} ${iphone_time_capture}(?P<stuff>.*)"
 
 whatsapp_match="${whatsapp_date_capture}(?P<stuff>.*)"
 
-replace_datetime='$+{year}_$+{month}_$+{day}_$+{hour}-$+{min}-$+{sec}'
+replace_datetime='$+{year}-$+{month}-$+{day}T$+{hour}$+{min}$+{sec}'
 
 rename "s/^$pixel_modes_match$/${replace_datetime}_$+{stuff}/s" "$@"
-rename "s/^$pixel_modes_match2$/$replace_datetime$+{stuff}/s" "$@"
+rename "s/^$pixel_modes_match2$/${replace_datetime}_$+{stuff}/s" "$@"
 rename "s/^$pixel_modes_match_screenshot$/${replace_datetime}_screenshot$+{stuff}/s" "$@"
-rename "s/^$iphone_match$/$replace_datetime$+{stuff}/s" "$@"
-#rename "s/^$whatsapp_match$/$+{year}_$+{month}_$+{day}$+{stuff}/s" "$@"
+rename "s/^$iphone_match$/${replace_datetime}_$+{stuff}/s" "$@"
+rename "s/^$whatsapp_match$/$+{year}_$+{month}_$+{day}$+{stuff}/s" "$@"
